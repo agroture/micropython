93874874a9f298c6698a86e10da5b97800f5d5ec
Amirreza Hamzavi <amirrezahamzavi2000@gmail.com>
2024-02-10 21:57:01 +0330

    Create board variants for esp32
    + This commit adds partition tables for maximum flash buffer usage within ESP32 and ESP32 C3 and ESP32 S3 production and development stages.
    + OTA variants are added.

--- ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
@@ -1,7 +1,20 @@
 // This configuration is for a generic ESP32C3 board with 4MiB (or more) of flash.
 
+#ifndef MICROPY_HW_BOARD_NAME
 #define MICROPY_HW_BOARD_NAME               "ESP32C3 module"
+#endif
+
+#ifndef MICROPY_HW_MCU_NAME
 #define MICROPY_HW_MCU_NAME                 "ESP32C3"
+#endif
+
+#ifndef MICROPY_HW_ENABLE_SDCARD
+#define MICROPY_HW_ENABLE_SDCARD            (0)
+#endif
+
+#ifndef MICROPY_PY_MACHINE_I2S
+#define MICROPY_PY_MACHINE_I2S              (0)
+#endif
 
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
---
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigvariant_BP_DEV_4MB.cmake
@@ -0,0 +1,9 @@
+set(SDKCONFIG_DEFAULTS
+    ${SDKCONFIG_DEFAULTS}
+    boards/sdkconfig.ble
+    boards/sdkconfig.bp4mb.dev
+)
+
+list(APPEND MICROPY_DEF_BOARD
+    MICROPY_HW_MCU_NAME="ESP32C3-BP-DEV"
+)
