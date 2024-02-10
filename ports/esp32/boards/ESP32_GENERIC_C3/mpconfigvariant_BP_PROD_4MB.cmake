c6818433b9d75c1fa2c96a24181b4e1fa49aea12
Amirreza Hamzavi <amirrezahamzavi2000@gmail.com>
2024-02-10 21:57:01 +0330

    Create board variants for esp32
    + This commit adds partition tables for maximum flash buffer usage within ESP32 and ESP32 C3 and ESP32 S3 production and development stages.
    + OTA variants are added.

---
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigvariant_BP_PROD_4MB.cmake
@@ -0,0 +1,9 @@
+set(SDKCONFIG_DEFAULTS
+    ${SDKCONFIG_DEFAULTS}
+    boards/sdkconfig.minimal
+    boards/sdkconfig.bp4mb.prod
+)
+
+list(APPEND MICROPY_DEF_BOARD
+    MICROPY_HW_MCU_NAME="ESP32C3-BP-PROD"
+)
