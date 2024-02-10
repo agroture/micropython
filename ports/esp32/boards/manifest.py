60052d98f1fe00452cfab985870b789a13d46cf7
Amirreza Hamzavi <amirrezahamzavi2000@gmail.com>
2024-02-10 21:57:01 +0330

    Create board variants for esp32

--- ports/esp32/boards/manifest.py
+++ ports/esp32/boards/manifest.py
@@ -6,10 +6,6 @@ require("bundle-networking")
 
 # Require some micropython-lib modules.
 require("aioespnow")
-require("dht")
 require("ds18x20")
-require("neopixel")
 require("onewire")
-require("umqtt.robust")
-require("umqtt.simple")
-require("upysh")
+require("cbor2")
