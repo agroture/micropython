cf15ef91d740238cdd2e36aa2678241f6667c4e0
Amirreza Hamzavi <amirrezahamzavi2000@gmail.com>
2025-05-07 01:47:25 +0330

    Wipe and recreate filesystem in case of corruption

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -2,12 +2,27 @@ import gc
 import vfs
 from flashbdev import bdev
 
-try:
-    if bdev:
-        vfs.mount(bdev, "/")
-except OSError:
-    import inisetup
 
-    inisetup.setup()
+def _erase_bdev():
+    from esp import flash_erase
+
+    i = bdev.info()
+    for s in range(i[2] // 4096, (i[2] + i[3]) // 4096):
+        flash_erase(s)
+
+
+if bdev:
+    try:
+        vfs.mount(bdev, "/")
+    except OSError:
+        _erase_bdev()
+        vfs.VfsLfs2.mkfs(bdev)
+        try:
+            vfs.mount(bdev, "/")
+        except OSError:
+            print("Failed to make filesystem after wiping")
+        else:
+            with open(".mkfs", "wb") as f:
+                pass
 
 gc.collect()
